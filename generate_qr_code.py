67ca1c5cb0c09a4db088deaa7f221e2f85d11620
vikassharma812 <59166212+vikassharma812@users.noreply.github.com>
2024-04-30 14:52:04 +0530

    Create generate_qr_code.py

---
+++ generate_qr_code.py
@@ -0,0 +1,21 @@
+import qrcode
+
+def generate_qr_code(data, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+if __name__ == "__main__":
+    # Example data for QR code
+    data = "Gurmeet Singh"
+
+    # Generate QR code image
+    generate_qr_code(data, "qrcode.png")
